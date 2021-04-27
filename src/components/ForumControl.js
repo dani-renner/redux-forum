cbf68eca85870f5a40b15081ee076c2f413b9a31
Nick Lindau <nicholaithegreat@gmail.com>
2021-04-27 15:58:37 -0700

    add toggle form functionality Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- src/components/ForumControl.js
+++ src/components/ForumControl.js
@@ -2,22 +2,49 @@ import React from "react";
 import PostList from "../components/Posts/PostList";
 import NewPostForm from "./Posts/NewPostForm";
 import NewCommentForm from "./Comments/NewCommentForm";
-
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 // import ReusableForm from "./ReuseableForm";
 
 class ForumControl extends React.Component {
   // constructor and methods go here
+  constructor(props) {
+    super(props);
+  }
+
+  handleClick = () => {
+    const { dispatch } = this.props;
+    const action = {
+      type: "TOGGLE_FORM"
+    }
+    dispatch(action);
+  }
+
   render() {
-    // logic for displaying current view goes here
+    let currentlyVisibleState = null;
+    if (this.props.formVisibleOnPage) {
+      currentlyVisibleState = <NewPostForm />
+    } else {
+      currentlyVisibleState = <PostList />
+    }
     return (
       // returning whichever view is selected goes here
       <React.Fragment>
-        <PostList />
-        <NewPostForm />
-        <NewCommentForm />
+        {currentlyVisibleState}
+
+        {/* <NewCommentForm /> */}
+        <button onClick={this.handleClick}>TOGGLE STATE</button>
       </React.Fragment>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    formVisibleOnPage: state.formVisibleOnPage
+  }
+}
+
+ForumControl = connect(mapStateToProps)(ForumControl);
+
 export default ForumControl;
